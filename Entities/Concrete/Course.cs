2bccae14a4f81ea958fdfa34b93c6353d828dfb5
Ahmet <Ahmet@DESKTOP-ESR8B2V>
2024-02-23 22:35:26 +0300

    Add project files.

---
+++ Entities/Concrete/Course.cs
@@ -0,0 +1,13 @@
+﻿using Course.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    internal class Course : IEntity
+    {
+    }
+}
---
+++ Entities/Concrete/Instructor.cs
@@ -0,0 +1,13 @@
+﻿using Course.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities.Concrete
+{
+    internal class Instructor : IEntity
+    {
+    }
+}
